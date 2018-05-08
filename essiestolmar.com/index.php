80ee414080b0fb947403c86dd521a297b94de6c5
szabigreen <szabigreenit@gmail.com>
2018-05-08 18:15:25 +0200

    index.php changed

--- essiestolmar.com/index.php
+++ essiestolmar.com/index.php
@@ -2,9 +2,5 @@
 <?php
 require('../vendor/autoload.php');
 $app['debug'] = true;
-// Our web handlers
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
+
 $app->run();

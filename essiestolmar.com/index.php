9f3771e02058a98812d1c58fef9cc826eb76454f
szabigreen <szabigreenit@gmail.com>
2018-05-09 13:50:12 +0200

    adding addons

--- essiestolmar.com/index.php
+++ essiestolmar.com/index.php
@@ -1,9 +1,22 @@
 
 <?php
 require('../vendor/autoload.php');
+
+$app = new Silex\Application()
 $app['debug'] = true;
 include_once("index-single-product.html");
 
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+// Register view rendering
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+// own
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return str_repeat('Hello', getenv('TIMES'));

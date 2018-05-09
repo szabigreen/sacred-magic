870bcd778bd0860e67b831c422de4279a8cd39af
szabigreen <szabigreenit@gmail.com>
2018-05-09 19:59:05 +0200

    editing index.php

--- essiestolmar.com/index.php
+++ essiestolmar.com/index.php
@@ -1,9 +1,7 @@
 <?php
 require('../vendor/autoload.php');
 $path = '../vendor/silex/silex/src/Silex'
-//$path = '/vendor/silex/src/Silex';
-set_include_path(get_include_path() . PATH_SEPARATOR . $path);
-use Application as SacredApp;
+use Silex\Application as SacredApp;
 $app = new SacredApp();
 $app['debug'] = true;
 include_once("index-single-product.html");

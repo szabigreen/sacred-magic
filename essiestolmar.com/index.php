590a8c3d70768f6875dcdf9c2ccb8883606adef9
szabigreen <szabigreenit@gmail.com>
2018-05-09 21:25:44 +0200

    editing index.php

--- essiestolmar.com/index.php
+++ essiestolmar.com/index.php
@@ -5,11 +5,7 @@ error_reporting(E_ALL);
 
 require __DIR__.'/../vendor/autoload.php';
 
-echo __DIR__.'../vendor/autoload.php';
-
 $app = new Silex\Application();
-
-$app['debug'] = true;
 
 //$app->get('/hello/{name}', function($name) use($app) {
 //    return 'Hello '.$app->escape($name);

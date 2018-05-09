50c04b803a84155cefac0ddb520841ce97e6c1dc
szabigreen <szabigreenit@gmail.com>
2018-05-09 13:39:39 +0200

    editing index.php

--- essiestolmar.com/index.php
+++ essiestolmar.com/index.php
@@ -3,5 +3,11 @@
 require('../vendor/autoload.php');
 $app['debug'] = true;
 include_once("index-single-product.html");
+
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return str_repeat('Hello', getenv('TIMES'));
+});
+
 ?>
 

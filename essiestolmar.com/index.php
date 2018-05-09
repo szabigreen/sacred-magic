c99037921800065dde1487e474e174f5f9658ea9
szabigreen <szabigreenit@gmail.com>
2018-05-09 22:16:33 +0200

    some change

--- essiestolmar.com/index.php
+++ essiestolmar.com/index.php
@@ -7,9 +7,9 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-//$app->get('/hello/{name}', function($name) use($app) {
-//    return 'Hello '.$app->escape($name);
-//});
+$app->get('/hello/{name}', function($name) use($app) {
+    return 'Hello '.$app->escape($name);
+});
 
 //$app->run();
 

73364e685c1fc1cf748421088ec4328caecc5f46
szabigreen <szabigreenit@gmail.com>
2018-05-17 00:53:10 +0200

    galery, pics added

--- essiestolmar.com/index.php
+++ essiestolmar.com/index.php
@@ -14,11 +14,38 @@ $app->get("/users/{id}", function($id){
    return "User - {$id}";
 });
 
-$app->get('/', function(){
+/*$app->get('/', function(){
 	return "Hello world";
 });
-
+**/
 
 $app->run();
 
 
+/*
+
+use Application as SacredApp;
+$app = new SacredApp();
+
+
+$app['debug'] = true;
+include_once("index-single-product.html");
+
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+// Register view rendering
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+// own
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return str_repeat('Hello', getenv('TIMES'));
+});
+
+$app->run();
+*/

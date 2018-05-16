da3f1ad20c831ba3b4d3db0827f98f5d451d1fd7
szabigreen <szabigreenit@gmail.com>
2018-05-17 01:04:25 +0200

    index change

--- essiestolmar.com/index.php
+++ essiestolmar.com/index.php
@@ -22,30 +22,3 @@ $app->get("/users/{id}", function($id){
 $app->run();
 
 
-/*
-
-use Application as SacredApp;
-$app = new SacredApp();
-
-
-$app['debug'] = true;
-include_once("index-single-product.html");
-
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// own
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return str_repeat('Hello', getenv('TIMES'));
-});
-
-$app->run();
-*/

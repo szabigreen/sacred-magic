0444c351578ab31bc74308139a13e88122a15b44
szabigreen <szabigreenit@gmail.com>
2018-06-01 20:10:01 +0200

    js replaced

--- essiestolmar.com/js/custom-plugin-setting/giman-instafeed-setting.js
+++ essiestolmar.com/js/custom-plugin-setting/giman-instafeed-setting.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 	================================*/
 	var feed = new Instafeed({
         get: 'user', // Get photos based on tag
-        accessToken: '1695826387.ba4c844.bac1962aa65f4e8fbff6c251e7954197', // Access Token, generate yours here http://instagramwordpress.rafsegat.com/docs/get-access-token/
+        accessToken: '1695826387.ba4c844.96e99c6fc0874718a5cdc9d167ac1484', // Access Token, generate yours here http://instagramwordpress.rafsegat.com/docs/get-access-token/
         userId: '1695826387', // User Id (The first row from access token, before the first dot (.) sign)
         template: '<li class="image-large"><a href="{{link}}" target="blank"><img src="{{image}}" class="image-gallery-item image-left full-width" /></a></li>',
         limit: 6, // Show only 6 photos
